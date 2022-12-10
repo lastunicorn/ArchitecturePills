3f710400371ca3c27beb2148ff3a23da0fc43543
Alexandru Iuga <alex.iuga@gmail.com>
2022-12-10 15:10:28 +0200

    code cleanunp

--- sources/ArchitecturePills.Bootstrapper/SetupServices.cs
+++ sources/ArchitecturePills.Bootstrapper/SetupServices.cs
@@ -19,16 +19,15 @@ using DustInTheWind.ArchitecturePills.DataAccess;
 using DustInTheWind.ArchitecturePills.Ports.DataAccess;
 using DustInTheWind.ArchitecturePills.Presentation;
 
-namespace DustInTheWind.ArchitecturePills.Bootstrapper
-{
-    internal static class SetupServices
-    {
-        public static void Configure(ContainerBuilder containerBuilder)
-        {
-            containerBuilder.RegisterType<InflationRepository>().As<IInflationRepository>();
+namespace DustInTheWind.ArchitecturePills.Bootstrapper;
 
-            containerBuilder.RegisterType<MainWindow>().AsSelf();
-            containerBuilder.RegisterType<MainViewModel>().AsSelf();
-        }
+internal static class SetupServices
+{
+    public static void Configure(ContainerBuilder containerBuilder)
+    {
+        containerBuilder.RegisterType<InflationRepository>().As<IInflationRepository>();
+
+        containerBuilder.RegisterType<MainWindow>().AsSelf();
+        containerBuilder.RegisterType<MainViewModel>().AsSelf();
     }
 }
